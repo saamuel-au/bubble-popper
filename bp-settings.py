d355ec9fe53a93cfd327ea9e982a02aadc89e405
JakobKaur <147378697+JakobKaur@users.noreply.github.com>
2024-09-17 12:54:44 +0300

    bp-settings.py

--- bp-settings.py
+++ bp-settings.py
@@ -7,7 +7,7 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (0, 0, 139)
-        self.caption = "Bubble Bluster"
+        self.caption = "Bubble Popper"
         
         self.bubble_min_r = 10
         self.bubble_max_r = 50
